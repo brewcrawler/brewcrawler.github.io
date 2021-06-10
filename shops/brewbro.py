7d67e9714650a51d3e7d5a2c3ab09931a1219ed6
elmerenges <elmerenges@users.noreply.github.com>
2021-06-10 12:24:00 +0200

    hop table ready

--- shops/brewbro.py
+++ shops/brewbro.py
@@ -35,6 +35,30 @@ def process_malts(soup):
             }
             malt_list.append(temp)
 
+def process_hops(soup):
+    hops = soup.find_all("div", class_="product-snapshot list_div_item")
+    for hop in hops:
+            name = hop.find('div', class_='snapshot-list-item list_prouctname').text.replace("\n", "")
+            #price = hop.find('div', class_='snapshot-list-item list_prouctprice').text.replace("\n", "").replace("    ", "")
+            price = hop.find('div', class_='snapshot-list-item list_prouctprice')
+            if price is None:
+                price = hop.find('div', class_='snapshot-list-item list_prouctprice list_productprice_special').find('span', class_='list_special').text.replace("\n", "")
+            else:
+                price = price.text.replace("\n", "")
+            if hop.find('td', class_='param-value featured-param featured-alfa_sav'):
+                aa = hop.find('td', class_='param-value featured-param featured-alfa_sav').text.replace("\n", "").replace("ALFA-SAV:", "").replace(" ", "")
+            else:
+                aa = ""
+
+            temp = {
+                "type" : "hop",
+                "name" : name,
+                "price" : price,
+                "aa" : aa,
+                "shop": SHOP
+            }
+            hop_list.append(temp)
+
 def crawl_malts():
     for malt_page in MALT_URL:
         page = requests.get(malt_page)
@@ -51,7 +75,19 @@ def crawl_malts():
             process_malts(soup)
 
 def crawl_hops():
-    pass
+    for hop_page in HOP_URL:
+        page = requests.get(hop_page)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        multiple_pages = soup.find_all("a", class_="pagination-link")
+        page_list = []
+        for mp in multiple_pages:
+            page_list.append(mp["href"])
+        page_list=list(dict.fromkeys(page_list))
+        process_hops(soup)
+        for other_page in page_list:
+            page = requests.get(other_page)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            process_hops(soup)
 
 def crawl_yeasts():
     pass
@@ -64,6 +100,8 @@ def dump_to_json(type, list):
 
 if __name__ == "__main__":
     crawl_malts()
+    crawl_hops()
     dump_to_json("malt", malt_list)
+    dump_to_json("hop", hop_list)
 
     
