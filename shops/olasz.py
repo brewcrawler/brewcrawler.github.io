4b76c2a3170b1f393af977fe2b2c6a797f6b79e5
elmerenges <elmerenges@users.noreply.github.com>
2021-06-10 15:50:33 +0200

    Update olasz.py

--- shops/olasz.py
+++ shops/olasz.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 
-MALT_URL = ["https://www.brewbro.hu/malatak/osszes-malata"]
-HOP_URL = ["https://www.brewbro.hu/komlok/osszes-komlo"]
-YEAST_URL = ["https://www.brewbro.hu/elesztok/osszes-eleszto"]
+MALT_URL = ["https://olaszsped.com/index.php?main_page=index&cPath=2_34&zenid=af55a770d733e764bd56ddd73629cd29"]
+HOP_URL = ["https://olaszsped.com/index.php?main_page=index&cPath=2_21&zenid=af55a770d733e764bd56ddd73629cd29"]
+YEAST_URL = ["https://olaszsped.com/index.php?main_page=index&cPath=2_19&zenid=af55a770d733e764bd56ddd73629cd29"]
 SHOP = "Olasz"
 
 page = None
@@ -15,23 +15,28 @@ hop_list = []
 yeast_list = []
 
 def process_malts(soup):
-    malts = soup.find_all("div", class_="product-snapshot list_div_item")
+    malts = soup.find_all("tr", class_="productListing-odd")
     for malt in malts:
-            name = malt.find('div', class_='snapshot-list-item list_prouctname').text.replace("\n", "")
-            price = malt.find('div', class_='snapshot-list-item list_unitprice').text.replace("\n", "").replace("    ", "")
-            if malt.find('td', class_='param-value featured-param featured-malata_ebc'):
-                ebc = malt.find('td', class_='param-value featured-param featured-malata_ebc').text.replace("\n", "").replace("EBC: ", "")
-            else:
-                ebc = ""
+            #print(malt)
+            name = malt.find_all('td', class_='productListing-data')
+            print(name)
+            #if malt.find('div', class_='productListing-data').find('h3', class_='itemTitle'):
+            #    name = malt.find('div', class_='productListing-data').find('h3', class_='itemTitle').find('a')
+            #    print(name)
+    #        price = malt.find('div', class_='snapshot-list-item list_unitprice').text.replace("\n", "").replace("    ", "")
+    #        if malt.find('td', class_='param-value featured-param featured-malata_ebc'):
+    #            ebc = malt.find('td', class_='param-value featured-param featured-malata_ebc').text.replace("\n", "").replace("EBC: ", "")
+    #        else:
+    #            ebc = ""
 
-            temp = {
-                "type" : "malt",
-                "name" : name,
-                "price" : price,
-                "ebc" : ebc,
-                "shop": SHOP
-            }
-            malt_list.append(temp)
+    #        temp = {
+    #            "type" : "malt",
+    #            "name" : name,
+    #            "price" : price,
+    #            "ebc" : ebc,
+    #            "shop": SHOP
+    #        }
+    #        malt_list.append(temp)
 
 def process_hops(soup):
     hops = soup.find_all("div", class_="product-snapshot list_div_item")
@@ -57,19 +62,15 @@ def process_hops(soup):
             hop_list.append(temp)
 
 def crawl_malts():
-    #for malt_page in MALT_URL:
-    #    page = requests.get(malt_page)
-    #    soup = BeautifulSoup(page.content, 'html.parser')
-    #    multiple_pages = soup.find_all("a", class_="pagination-link")
-    #    page_list = []
-    #    for mp in multiple_pages:
-    #        page_list.append(mp["href"])
-    #    page_list=list(dict.fromkeys(page_list))
-    #    process_malts(soup)
-    #    for other_page in page_list:
-    #        page = requests.get(other_page)
-    #        soup = BeautifulSoup(page.content, 'html.parser')
-    #        process_malts(soup)
+    for malt_page in MALT_URL:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+        }
+        page = requests.get(malt_page, headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        #clear
+        #print(soup)
+        process_malts(soup)
     return malt_list
 
 def crawl_hops():
@@ -93,15 +94,12 @@ def crawl_yeasts():
     return yeast_list
     pass
 
-def main():
-    print("hello")
-
 if __name__ == "__main__":
-    #crawl_malts()
+    crawl_malts()
     #crawl_hops()
     #crawl_yeasts()
     #dump_to_json("malt", malt_list)
     #dump_to_json("hop", hop_list)
-    main()
+    pass
 
     
